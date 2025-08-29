38e45768968fa8b22ecfa51de49a4019d05a99a0
Bengisu <bengisu.161@gmail.com>
2025-08-29 12:10:31 +0300

    SP-6

--- src/main/java/com/bengisu/springProje/repository/BookRepository.java
+++ src/main/java/com/bengisu/springProje/repository/BookRepository.java
@@ -1,8 +1,6 @@
 package com.bengisu.springProje.repository;
 
 import com.bengisu.springProje.model.Book;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
