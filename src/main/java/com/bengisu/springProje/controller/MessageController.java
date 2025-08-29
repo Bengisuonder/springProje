ef528f3d52ec8146b407c12b3e3dbf74a4119e76
Bengisu <bengisu.161@gmail.com>
2025-08-29 09:44:51 +0300

    SP-4

--- src/main/java/com/bengisu/springProje/controller/MessageController.java
+++ src/main/java/com/bengisu/springProje/controller/MessageController.java
@@ -1,9 +1,19 @@
 package com.bengisu.springProje.controller;
 
+import com.bengisu.springProje.service.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MessageController
 {
+    @Autowired
+    private MessageService messageService;
 
+    @GetMapping("/message")
+    public String message()
+    {
+        return messageService.getMessage();
+    }
 }
